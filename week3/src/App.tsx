c60d87864f0a227941b7036b2fe975f6657f3928
Basliel <baslielz727@gmail.com>
2026-03-29 13:45:13 +0300

    Week3 warmup

---
+++ week3/src/App.tsx
@@ -0,0 +1,15 @@
+import { AppleBasket } from "./components/AppleBasket"
+import { AppleButton } from "./components/AppleButton"
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-black text-xl">Welcome</h1>
+    <AppleButton/>
+    <AppleBasket/>
+    </>
+  )
+}
+
+export default App
